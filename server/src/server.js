44487ef8bf4006212955215313989933708a474b
Onopriienko Bohdan <lkenn125@gmail.com>
2024-07-06 23:58:29 +0300

    minor updates

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-// const port = 3000
 
 //в докере не нужно будет писать такой путь, файл будет лежать рядом
 require('dotenv').config({ path: `${__dirname}/../../.env` })
