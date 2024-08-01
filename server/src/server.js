a59688fa00c6e873b6c590dcd584c8a1e2b83584
Onopriienko Bohdan <lkenn125@gmail.com>
2024-08-01 19:40:10 +0300

    Created variouses configs for dev/prod builds

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
-const PORT = process.env.PORT || 3000;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+var app = (0, express_1.default)();
+var PORT = process.env.PORT || 3000;
 console.log('Current working directory:', process.cwd());
-
-
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-
-const server = app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
+var server = app.listen(PORT, function () {
+    console.log("Example app listening on port ".concat(PORT));
 });
+exports.default = server;
